15ad8a2923adf1455357c048aebe4786d143f7fc
Sathya <neelasathya06@gmail.com>
2025-05-18 16:38:51 +0530

    hooks implemented -- useState implemented on another component

---
+++ src/App_UseState.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import './App.css'
+
+function App(){
+
+    const [counters, setCounters] = useState([{id: 1, value: 0}]);
+    const addCounter = () => {
+            setCounters([...counters, {id: counters.length + 1, value: 0}])
+    };
+
+    const incrementCounter = (id) => {
+        setCounters(counters.map(counter => counter.id === id
+            ? {...counter, value: counter.value + 1}
+            : counter))
+    };
+
+    return (
+        <div>
+            <button onClick={addCounter}>
+                Add Counter
+            </button>
+            <ul>
+                {counters.map(counter => (
+                    <li key={counter.id}>
+                        Counter {counter.id} : {counter.value}
+                        <button onClick={() => {incrementCounter(counter.id)}}>Increment</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default App;
