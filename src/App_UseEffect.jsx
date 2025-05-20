9264534ed73655d0dafe198ee5a6849ed78479ca
Sathya <neelasathya06@gmail.com>
2025-05-20 11:09:51 +0530

    useEffect with dependency options with example implemented!

--- src/App_UseEffect.jsx
+++ src/App_UseEffect.jsx
@@ -4,9 +4,11 @@ import './apps_css/App_UseEffect.css'
 function App(){
 
     const [count, setCount] = useState(0);
+    const [anotherValue, setAnotherValue] = useState(10);
 
-    useEffect(()=>{
+    useEffect(()=> {
         document.title = `Count: ${count}`
+        console.log(`UseEffect triggered!`);
     },[count]);
 
     const incrementCount = () => {
@@ -19,6 +21,7 @@ function App(){
         <div>
             <h1>UseEffect Hook</h1>
             <button onClick={incrementCount}>Increment</button>
+            <button onClick={() => setAnotherValue(anotherValue+1)}>Increment_10</button>
         </div>
     )
 }
