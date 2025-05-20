c65c54fabaef52405ca8a7aefde8415503964469
Sathya <neelasathya06@gmail.com>
2025-05-20 11:17:00 +0530

    mouseTracker implemented using useEffect impl

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 // import App from './App_UseState.jsx'
 // import App from './App_Forms.jsx'
 // import App from './App_ColorPicket.jsx'
-import App from './App_UseEffect.jsx'
+// import App from './App_UseEffect.jsx'
+import App from './App_MouseTracker.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
